26e8831afd317a601f2c459d8ca9413e755f213c
yuhenglai <104549250+yuhenglai@users.noreply.github.com>
2024-11-16 21:21:21 -0600

    updated Rcpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -43,7 +43,7 @@ RcppExport SEXP _PseudotimeDE_eigenForDiscreteProbs(SEXP pSEXP) {
     if (rcpp_isError_gen) {
         SEXP rcpp_msgSEXP_gen = Rf_asChar(rcpp_result_gen);
         UNPROTECT(1);
-        Rf_error(CHAR(rcpp_msgSEXP_gen));
+        Rf_error("%s", CHAR(rcpp_msgSEXP_gen));
     }
     UNPROTECT(1);
     return rcpp_result_gen;
@@ -79,7 +79,7 @@ RcppExport SEXP _PseudotimeDE_HoeffIndMixedCdfRCPP(SEXP xSEXP, SEXP eigenPSEXP, 
     if (rcpp_isError_gen) {
         SEXP rcpp_msgSEXP_gen = Rf_asChar(rcpp_result_gen);
         UNPROTECT(1);
-        Rf_error(CHAR(rcpp_msgSEXP_gen));
+        Rf_error("%s", CHAR(rcpp_msgSEXP_gen));
     }
     UNPROTECT(1);
     return rcpp_result_gen;
@@ -114,7 +114,7 @@ RcppExport SEXP _PseudotimeDE_TStarHellerAndHellerRCPP(SEXP xSEXP, SEXP ySEXP) {
     if (rcpp_isError_gen) {
         SEXP rcpp_msgSEXP_gen = Rf_asChar(rcpp_result_gen);
         UNPROTECT(1);
-        Rf_error(CHAR(rcpp_msgSEXP_gen));
+        Rf_error("%s", CHAR(rcpp_msgSEXP_gen));
     }
     UNPROTECT(1);
     return rcpp_result_gen;
